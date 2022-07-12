c6d7821dd19b9aff711ced436ad73f3b4294e4e1
mollani <mollani0704@gmail.com>
2022-07-12 20:53:13 +0900

    init commit

---
+++ src/main/java/com/springboot/studyjongseong/StudyJongseongApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.studyjongseong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyJongseongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyJongseongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/studyjongseong/StudyJongseongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.studyjongseong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyJongseongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
